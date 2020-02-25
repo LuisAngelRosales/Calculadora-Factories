0ad4c271a02710bf182fbdad515b4e1116871dc9
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-02-24 18:41:43 -0600

    Create ListFactory.java

---
+++ src/application/ListFactory.java
@@ -0,0 +1,19 @@
+package application;
+
+// DANIELA VILLAMAR 19086//
+// LUIS ROSALES//
+
+class ListFactory<E> {
+
+	public AbstractList<E> getList(String entry) {
+    // seleccion de la implementacion a utilizar:
+	   if(entry.equals("Lista Simple")) {
+			return new SinglyLinkedList<E>();
+		}else if(entry.equals("Lista Doble")) {
+			return new DoublyLinkedList<E>();
+		}else {
+			return new CircularList<E>();
+		}
+   }
+
+}
