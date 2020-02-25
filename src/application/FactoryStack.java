4873ddbba73a5308d1c371b9e41489aef501a736
wicho <wicho@DESKTOP-OONL9P1>
2020-02-25 11:33:31 -0600

    Correccion a factory

---
+++ src/application/FactoryStack.java
@@ -0,0 +1,19 @@
+public class FactoryStack<E> {
+	
+	public iStack<E> getStack(String implementacion) {
+		if(implementacion == null) {
+			return new ImplementacionStackDefault<E>();
+		}
+		if(implementacion.equalsIgnoreCase("Vector")) {
+			return new StackVector<E>();
+		}
+		if(implementacion.equalsIgnoreCase("ArrayList")) {
+			return new Stack<E>();
+		}
+		if(implementacion.equalsIgnoreCase("List")) {
+			return null;
+		}
+		return new ImplementacionStackDefault<E>();
+	}
+
+}
